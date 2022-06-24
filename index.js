ef7551cc3bcfec8f19d95589dd923f2deedb2f5f
MrBean <1085616499@qq.com>
2022-06-24 11:49:19 +0800

    update
    update

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 
 
+
 const express = require('express')
-const app = express()
-// body-parser是一个http请求体解析的中间件，使用这个模块可以解析json\raw\文本\url
-const bodyParser = require('body-parser')
+const path = require('path')
 const router = require('./router')
 const port = 3008
- 
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 // 通过如下代码就可以将 public 目录下的图片、CSS 文件、JavaScript 文件对外开放访问了：
-app.use(express.static('public'))
+// app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, './public')))
 // 处理请求参数 跨域问题
 app.all('*',function(req,res,next){
   res.header("Access-Control-Allow-Origin","*")
@@ -22,3 +24,4 @@ app.use(router)
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
